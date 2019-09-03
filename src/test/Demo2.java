9cdce7a672346bc0a8dde6e511d8c1eb4530957b
Lijunchao79 <1044574481@qq.com>
2019-09-03 17:31:37 +0800

    first commit

---
+++ src/test/Demo2.java
@@ -0,0 +1,10 @@
+package test;
+import java.util.Scanner;
+public class Demo2 {
+	public static void main(String[] args) 
+	{
+		Scanner scanner = new Scanner(System.in);
+		int i = scanner.nextInt();
+		System.out.println(i);
+	}
+}
